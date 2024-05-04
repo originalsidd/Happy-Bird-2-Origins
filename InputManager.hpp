3c003e7fb21a45149ed8fd2e1b46dae7ea4e6ae4
originalsidd <originalsidd@gmail.com>
2024-05-04 23:58:25 +0530

    Basic Engine

---
+++ InputManager.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+
+namespace Cosmic
+{
+    class InputManager
+    {
+    public:
+        InputManager() {}
+        ~InputManager() {}
+        
+        bool isSpriteClicked(sf::Sprite object, sf::Mouse::Button button, sf::RenderWindow &window);
+        
+        sf::Vector2i GetMousePosition(sf::RenderWindow &window);
+    };
+}
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "Game.hpp"
+
+int main()
+{
+    Cosmic::Game(800, 600, "SFML Starter");
+    
+    return EXIT_SUCCESS;
+}
